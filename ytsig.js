dbb58def8dab196378cd13930b2de2fc9ab2196e
Artom Lifshitz <alifshit@redhat.com>
2024-04-08 15:49:27 -0400

    Use MutationObserver to replace page contents
    YouTube does some sort of JS black magic that doesn't actually
    load a page when you click on a Short - so while typing in a Short
    URL, or pressing Reload with a Short playing, replaceds the page
    content as expected, clicking on a Short from any other YouTube page
    didn't, because content_script that this extension injects didn't
    actually run.
    
    To fix thix, modify ytsig to install a MutationObserver into every
    youtube.com/* page, and use its callback to do our thing.

--- ytsig.js
+++ ytsig.js
@@ -1,12 +1,50 @@
-document.body.textContent = "";
-let phrases = [
-    "Don't rot your brain.",
-    "Why not do something productive, like take a nap?",
-    "You could disable me, but you're better than that."
-]
-let header = document.createElement("h1");
-let phrase = phrases[Math.floor(Math.random() * phrases.length)];
-header.textContent = phrase;
-content = document.createElement("div");
-content.appendChild(header);
-document.body.appendChild(content);
+let oldHref = document.location.href;
+
+let observer = new MutationObserver(function(mutations) {
+
+    let newHref = document.location.href;
+
+    if (newHref != oldHref && newHref.includes("shorts")) {
+
+        oldHref = newHref;
+
+        const phrases = [
+            "Don't rot your brain.",
+            "Why not do something productive, like take a nap?",
+            "You could disable me, but you're better than that."
+        ];
+        let phrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+        document.head.textContent = "";
+        let style = document.createElement("style");
+        style.textContent = `
+            div {
+                background-color: #3A643E;
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            h1 {
+                font: sans-serif;
+                letter-spacing: .1em;
+                font-size: 4em;
+                color: #E0E0CA;
+                text-align: center;
+            }`;
+        document.head.appendChild(style);
+
+        document.body.textContent = "";
+        let header = document.createElement("h1");
+        header.textContent = phrase;
+        let content = document.createElement("div");
+        content.appendChild(header);
+        document.body.appendChild(content);
+    };
+});
+observer.observe(document.body, { childList: true, subtree: true });
